9d518e81fcde1c7ff0512d225a6471db8c941f34
Yoochang <fluency1995@gmail.com>
2022-06-28 12:39:29 +1000

    .

---
+++ Linked List/File1/ListMain.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include "ArrayList.h"
+
+int main(void)
+{
+
+    // ArrayList의 생성 및 초기화
+    List list;
+    int data;
+    ListInit(&list);
+
+    // 5개의 데이터 저장
+    LInsert(&list, 4);
+    LInsert(&list, 22);
+    LInsert(&list, 42);
+    LInsert(&list, 22);
+    LInsert(&list, 14);
+
+    //저장된 데이터의 전체 출력
+    printf("현재 데이터의 수: %d\n", LCount(&list));
+
+    if (LFirst(&list, &data))
+    { //첫 번째 데이터 조회
+        printf("%d ", data);
+
+        while (LNext(&list, &data))
+            printf("%d ", data);
+    }
+    printf("\n\n");
+    //숫자 22를 탐색 후 모두 삭제
+    if (LFirst(&list, &data))
+    {
+        if (data == 22)
+            LRemove(&list);
+        while (LNext(&list, &data))
+        {
+            if (data == 22)
+                LRemove(&list);
+        }
+    }
+
+    //삭제 후 남은 데이터 전체 출력
+    printf("현재 데이터의 수 : %d\n", LCount(&list));
+
+    if (LFirst(&list, &data))
+    {
+        printf("%d ", data);
+
+        while (LNext(&list, &data))
+            printf("%d ", data);
+    }
+    printf("\n\n");
+    return 0;
+}
