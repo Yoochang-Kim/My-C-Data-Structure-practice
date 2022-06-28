697e31aae533d5fc0184e22549e1036f43c4621d
Yoochang <fluency1995@gmail.com>
2022-06-28 20:49:24 +1000

    .

---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char *name = "Yoochang Kim";
+    char name2[20] = "Yoochang Kim";
+    printf("%c\n",name[4]);
+    printf("%c\n",name2[0]);
+
+    return 0;
+}
