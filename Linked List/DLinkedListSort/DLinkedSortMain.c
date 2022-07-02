22929be3714e57a09baca303b0aa6b3f87d9ca62
Yoochang <fluency1995@gmail.com>
2022-07-02 18:23:22 +1000

    .

--- Linked List/DLinkedListSort/DLinkedSortMain.c
+++ Linked List/DLinkedListSort/DLinkedSortMain.c
@@ -9,3 +9,29 @@ int WhoIsPrecede(int d1, int d2)
     else
         return -1;
 }
+
+int main()
+{
+    List list;
+    int data;
+    ListInit(&list);
+
+    SetSortRule(&list,WhoIsPrecede); //정렬의 기준 등록
+
+    LInsert(&list,11);
+    LInsert(&list,22);
+    LInsert(&list,11);
+    LInsert(&list,22);
+    LInsert(&list,33);
+
+    printf("현재 데이터 수 : %d\n",LCount(&list));
+
+    if(LFirst(&list,data))
+    {
+        printf("%d ",data);
+
+        while(LNext(&list,&data))
+            printf("%d ", data);
+    }
+    printf("\n\n")
+}
