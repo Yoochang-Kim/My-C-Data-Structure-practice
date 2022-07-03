7bb6c651e1b20e1f4c8ded1228b7c296372ea8a0
Yoochang <fluency1995@gmail.com>
2022-07-03 23:50:07 +1000

    .

--- Linked List/CircularLinkedList/CLinkedListMain.c
+++ Linked List/CircularLinkedList/CLinkedListMain.c
@@ -28,6 +28,29 @@ int main(void)
     printf("\n");
 
     /* 2의 배수를 찾아서 모두 삭제 */
+    nodeNum = LCount(&list);
 
+    if(nodeNum != 0 )
+    {
+        LFirst(&list, &data);
+        if(data%2 == 0)
+            LRemove(&list);
+        
+        for(i=0; i<nodeNum-1; i++)
+        {
+            LNext(&list,&data);
+            if(data%2 ==0)
+                LRemove(&list);
+        }
+    }
     /* 전체 데이터 1회 출력 */
+    if(LFirst(&list, &data))
+    {
+        printf("%d ", data);
+
+        for(i=0;i<LCount(&list)-1;i++){
+            if(LNext(&list,&data))
+                printf("%d ",data);
+        }
+    }
 }
