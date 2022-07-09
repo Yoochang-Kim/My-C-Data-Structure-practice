e866c8f2c69ad92222ba51e5f9ddb96daded96da
Yoochang Kim <72631634+Yoochang-Kim@users.noreply.github.com>
2022-07-09 17:33:12 +1000

    .

--- Linked List/DoublyLinkedList/DBLinkedList.c
+++ Linked List/DoublyLinkedList/DBLinkedList.c
@@ -2,61 +2,61 @@
 #include <stdlib.h>
 #include "DBLinkedList.h"
 
-void ListInit(List * plist)
+void ListInit(List *plist)
 {
     plist->head = NULL;
     plist->numOfData = 0;
 }
 
-void LInsert(List * plist, Data data)
+void LInsert(List *plist, Data data)
 {
-    Node * newNode = (Node*)malloc(sizeof(Node));
+    Node *newNode = (Node *)malloc(sizeof(Node));
     newNode->data = data;
 
     newNode->next = plist->head;
-    if(plist->head !=NULL)
+    if (plist->head != NULL)
     {
         plist->head->prev = newNode;
     }
-    newNode ->prev = NULL;
+    newNode->prev = NULL;
     plist->head = newNode;
 
     (plist->numOfData)++;
 }
 
-int LFirst(List * plist, Data * pdata)
+int LFirst(List *plist, Data *pdata)
 {
-    if(plist->head ==NULL)
+    if (plist->head == NULL)
         return FALSE;
-    
+
     plist->cur = plist->head;
-    *pdata = plist->cur ->data;
+    *pdata = plist->cur->data;
     return TRUE;
 }
 
-int LNext(List *plist, Data * pdata)
+int LNext(List *plist, Data *pdata)
 {
-    if(plist->head == NULL)
+    if (plist->cur->next == NULL)
         return FALSE;
-    
+
     plist->cur = plist->cur->next;
     *pdata = plist->cur->data;
-    
+
     return TRUE;
 }
 
 int LPrevious(List *plist, Data *pdata)
 {
-    if(plist->head->prev == NULL)
+    if (plist->cur->prev == NULL)
         return FALSE;
-    
+
     plist->cur = plist->cur->prev;
     *pdata = plist->cur->data;
 
     return TRUE;
 }
 
-int LCount(List * plist)
+int LCount(List *plist)
 {
     return plist->numOfData;
 }
--- Linked List/DoublyLinkedList/DBLinkedListMain.c
+++ Linked List/DoublyLinkedList/DBLinkedListMain.c
@@ -9,27 +9,30 @@ int main(void)
     Data data;
 
     /* 8개의 데이터 생성 */
-    LInsert(&list,1);
-    LInsert(&list,2);
-    LInsert(&list,3);
-    LInsert(&list,4);
-    LInsert(&list,5);
-    LInsert(&list,6);
-    LInsert(&list,7);
-    LInsert(&list,8);
+    LInsert(&list, 1);
+    LInsert(&list, 2);
+    LInsert(&list, 3);
+    LInsert(&list, 4);
+    LInsert(&list, 5);
+    LInsert(&list, 6);
+    LInsert(&list, 7);
+    LInsert(&list, 8);
 
     /* 저장된 데이터의 조회 */
-    if(LFirst(&list,&data))
+    if (LFirst(&list, &data))
     {
         printf("%d ", data);
-    /* 오른쪽 노드로 이동하며 데이터 조회 */
-        while(LNext(&list,&data))
+        /* 오른쪽 노드로 이동하며 데이터 조회 */
+        while (LNext(&list, &data))
         {
             printf("%d ", data);
         }
-    
-    /* 왼쪽 노드로 이동하며 데이터 조회 */
-        
+
+        /* 왼쪽 노드로 이동하며 데이터 조회 */
+        while (LPrevious(&list, &data))
+        {
+            printf("%d ", data);
+        }
     }
 
     return 0;
